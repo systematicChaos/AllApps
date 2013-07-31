1a633c45731b9a840b04e843821d612351e68fe9
testest1 <myregpoint@gmail.com>
2013-07-31 21:40:13 +0300

    web testest1

--- PlanMyDay/PlanMyDay/viewControllers/tasksView/TTTasksTableViewCell.m
+++ PlanMyDay/PlanMyDay/viewControllers/tasksView/TTTasksTableViewCell.m
@@ -24,6 +24,7 @@
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
+    //added by testest web
     //added new changes by me
     //added changes by testest1!
     //add changes
