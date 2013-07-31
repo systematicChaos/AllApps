36a84d6544d32388d11d74730c96d27650183c70
systematicChaos <my_regmail@mail.ru>
2013-07-31 21:30:17 +0300

    xcode commit

--- PlanMyDay/PlanMyDay/viewControllers/tasksView/TTTasksTableViewCell.m
+++ PlanMyDay/PlanMyDay/viewControllers/tasksView/TTTasksTableViewCell.m
@@ -24,6 +24,7 @@
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
+    //added new changes by me
     //added changes by testest1!
     //add changes
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
